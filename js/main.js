8f6ee4c32d159caef007b9e40f371b0d2dc9cd1c
khe4oyan <khe4oyan@bk.ru>
2023-03-10 13:42:53 +0400

    texture fix

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ promo_add('Gold', 9_900);
 promo_add('NCoin', 10); 
 
 // ==- CRAFT'a add -====================
-craft_add('Gold Box', 'Box', 'Gold', 1, 1, 1, 'box_gold', 'Box', 'gold_material', 10_100);
-craft_add('Gold Car', 'Car', 'Gold', 1, 1, 2, 'car_gold', 'Car', 'gold_material', 23_000);
+craft_add('Gold Box', 'Box', 'Gold', 1, 1, 1, 'box_gold', 'box', 'gold_material', 10_100);
+craft_add('Gold Car', 'Car', 'Gold', 1, 1, 2, 'car_gold', 'car', 'gold_material', 23_000);
 
 // ==- GENERAL -====================
 let storage = new Storage();
