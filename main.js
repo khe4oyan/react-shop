72bdfc019d30e3f4607bf3aaad6d998bfb923794
khe4oyan <khe4oyan@bk.ru>
2023-03-21 02:06:04 +0400

    add craft, item, case

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ craft_add('Phone(C)', 'Phone', 'Case(blue)', 1, 1, 1, 'phone_in_blue_case', 'pho
 craft_add('Case(G)', 'Case(blue)', 'Gold', 1, 1, 1, 'phone_case_gold', 'phone_case_blue', 'gold_material', 10_000);
 craft_add('Phone(G)', 'Phone', 'Case(G)', 1, 1, 1, 'phone_in_gold_case', 'phone', 'phone_case_gold', 10_080);
 craft_add('#Phone(G)', 'Phone(C)', 'Gold', 1, 1, 1, 'phone_in_gold_case', 'phone_in_blue_case', 'gold_material', 10_080);
+craft_add('RTX videocard', 'Videocard', 'RTX fragment', 1, 1, 10, 'rtx_videocard', 'videocard', 'rtx_fragment', 200_000);
+craft_add('RTX gold box', 'Gold Box', 'RTX videocard', 1, 1, 1, 'rtx_box_gold', 'box_gold', 'rtx_videocard', 300_000);
+craft_add('RTX phone', 'Phone', 'RTX videocard', 1, 1, 1, 'rtx_phone', 'phone', 'rtx_videocard', 210_000);
+craft_add('RTX fish', 'Fish', 'RTX videocard', 1, 1, 1, 'rtx_fish', 'fish', 'rtx_videocard', 150_000);
 
 // ==- GENERAL -====================
 let storage = new Storage();
@@ -34,22 +38,27 @@ CTRL.add('Flower', 'flower', 5, null);
 CTRL.add('Vase', 'vase', 7, null);
 CTRL.add('Box', 'box', 10, null);
 CTRL.add('Phone', 'phone', 50, null);
-CTRL.add('UFO', 'ufo', 999_999, 1_000);
-CTRL.add('Car', 'car', 2_000, 1_000_000);
 CTRL.add('Case(blue)', 'phone_case_blue', 20, null);
+CTRL.add('UFO', 'ufo', 999_999, 1_000);
+CTRL.add('Videocard', 'videocard', 50_000, 500_000);
+CTRL.add('Car', 'car', 2_000, 1_000_000);
 CTRL.show();
 
 CTRL.create('Materials', 'shop_case', 8_000);
 CTRL.add('Gold', 'gold_material', 9_900, 500_000);
 CTRL.show();
 
+CTRL.create('RTX', 'rtx_shop_case', 10_000);
+CTRL.add('Box', 'box', 10, null);
+CTRL.add('RTX fragment', 'rtx_fragment', 20_000, 100_000);
+CTRL.add('RTX videocard', 'rtx_fragment', 200_000, 5_000);
+CTRL.show();
 
 
 // ==- SHOP ITEM's -====================
 // shop_add(name, img, price);
 // shop_add();
 
-
 // ==- TEST TOOL's -====================
-// random_chance_test(1_000_000, Item_case.max_chance);
-// prcent_calculate(15, Item_case.max_chance);
+random_chance_test(500_000, Item_case.max_chance);
+prcent_calculate(.005, Item_case.max_chance);
